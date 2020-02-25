5725aa4a28ae6442c33e3c4e60b8e8ef05173702
unknown <anujshashimal456@gmail.com>
2020-02-25 15:59:33 +0530

    Modified - 02

--- handler.js
+++ handler.js
@@ -1,80 +1,89 @@
 'use strict';
 const AWS = require('aws-sdk');
+const CognitoSDK = require('amazon-cognito-identity-js')
+AWS.CognitoIdentityServiceProvider.CognitoUserPool = CognitoSDK.CognitoUserPool;
+AWS.CognitoIdentityServiceProvider.CognitoUser = CognitoSDK.CognitoUser;
+
 const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 const uuid = require('uuid/v4');
 const postsTable = process.env.POSTS_TABLE;
-var attributeList = [];
-const AWSCognito = require('amazon-cognito-identity-js')
+const attributeList = [];
+// const CognitoUserAttribute = require('amazon-cognito-identity-js')
+module.exports.testing = function(event) {  
+  try {
+  const poolData = {
+    UserPoolId: 'us-east-1_7KRHMq0Kz',
+    ClientId: '517j0l4fd970grm1oub46mrm1p'
+  };
 
-const poolData = {
-  UserPoolId: 'us-east-1_nDfxWYKwM',
-  ClientId: '1fl22t1t1q58g2rjlppv97e0nh'
-};
+  const data = event.body;
+  const userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
 
-module.exports.testing = () => {  
-var name = {
-  Name : 'name',
-  Value : 'anuj' 
-};
+var username = {
+  Name : 'username',
+  Value : data.name
+  };
 var email = {
   Name : 'email',
-  Value : 'anuj@gmail.com' 
-};
+  Value : data.email
+  };
 var password = {
   Name : 'password',
-  Value : 'anuj321' 
-};
-var attributeName = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(name);
-var attributeEmail = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(email);
-var attributePassword = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(password);
+  Value : data.password
+  };
+
+const attributeName = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(username);
+const attributeEmail = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(email);
+const attributePassword = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(password);
 
 attributeList.push(attributeName);
 attributeList.push(attributeEmail);
 attributeList.push(attributePassword);
 var cognitoUser;
+userPool.signUp('username','email', 'password', attributeList, null, function(err, result){
+  if (err) {alert(err);return;}
+  cognitoUser = result.user;
+});
 
-userPool.signUp('email', 'password', attributeList, null, function(err, result){
-    if (err) {
-        alert(err);
-        return;}
-    cognitoUser = result.user;
-    console.log('user name is ' + cognitoUser.getUsername());
-});}
+  } catch (error) {
+  console.log(error)
+
+}
 
   module.exports.signup = async (callback) => {
-    const data = JSON.parse(event.body);
+ try{
+    const data = event.body;
     CognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
-    
     if (!data.name || data.name.trim() === '' || !data.email || data.email.trim() === '' || !data.password || data.password.trim() === '' ) {
-      return callback(null,response(400, {error: 'User must enter you name and email and they must not be empty'}));
-    }
+      return callback(null,response(400, {error: 'User must enter you name and email and they must not be empty'}));}
     else {
-        try {
+      try {
           attributeList.push(AWS.cognitoUserAttribute({
-          Name: 'name',
-          Value: name,
+          Name: 'username',
+          Value: data.name,
         })), 
         attributeList.push(AWS.cognitoUserAttribute({
           Name: 'email',
-          Value: email,
+          Value: data.email,
         })),
         attributeList.push(AWS.cognitoUserAttribute({
           Name: 'password',
-          Value: password,
+          Value: data.password,
         }));
       const userPool = AWS.cognitoUserPool(poolData);
       userPool.signUp(attributeList);
     } catch (error) {
-      console.log(error)
-      throw error;
-    }
+      return responseHelper.sendError(500, `Error`, error);
 
+    }
     // const addUser = await AWS.CognitoUserPool.createUser();
     if (addUser) { callback({statusMessage: util.statusMessage.USER_REGISTERED_SUCCESSFULLY,result : addUser});return;}
     else {callback({statusMessage: util.statusMessage.SERVER_BUSY});return;}
     } 
+  }catch (error){
+    return responseHelper.sendError(500, `Error`, error);
   }
-
+  }
 function response(statusCode, message) {
   return {
     statusCode: statusCode,
@@ -82,46 +91,51 @@ function response(statusCode, message) {
   };
 }
 
-// module.exports.login = async (event, context, callback) => {
-//   const data = JSON.parse(event.body);
-//   const userData = {
-//     email = data.email,
-//     password = data.password
-//   }
-
-//   if (!data.email || !data.password){
-//     return callback( null, response(400, {error: 'You must enter the valid Username and Password'}))}
-//   else{
-//     const chkUser = await AWS.CognitoUserPool.fetchUser(userData);
-//     if(chkUser){ callback(null, "You Successfually logged to the System")} 
-//     else{ callback(null, module.exports.signup)}
-//   }
-// }
-// Creating a user
-module.exports.createUser = (event, context, callback) => {
-  const reqBody = JSON.parse(event.body);
-
-  if (!reqBody.name || reqBody.name.trim() === '' || !reqBody.email || reqBody.email.trim() === '' || !reqBody.password || reqBody.password.trim() === '' ) {
-    return callback(null,response(400, {error: 'Post must enter you name and email and they must not be empty'}));
-    }
-
-  const post = {
-    id: uuid(),
-    createdAt: new Date().toISOString(),
-    userId: 1,
-    name: reqBody.name,
-    email: reqBody.email,
-    password: reqBody.password,
+module.exports.login = async (event, context) => {
+  const data = JSON.parse(event.body);
+  const poolData = {
+    UserPoolId: 'us-east-1_7KRHMq0Kz',
+    ClientId: '517j0l4fd970grm1oub46mrm1p'
   };
-  
-  return db.put({
-      TableName: postsTable,
-      Item: post
-    }).promise().then(() => {
-      callback(null, response(201, post));
-    }).catch((err) => response(null, response(err.statusCode, err)));
 
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+var userData = {
+	Email: data.email,
+	Pool: userPool,
 };
+
+var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+cognitoUser.confirmRegistration('123456', true, function(err, result) {
+	if (err) {
+		alert(err.message || JSON.stringify(err));
+		return;}
+	console.log('call result: ' + result);
+})}
+
+// Creating a user
+// module.exports.createUser = (event, context, callback) => {
+//   const reqBody = JSON.parse(event.body);
+
+//   if (!reqBody.name || reqBody.name.trim() === '' || !reqBody.email || reqBody.email.trim() === '' || !reqBody.password || reqBody.password.trim() === '' ) {
+//     return callback(null,response(400, {error: 'Post must enter you name and email and they must not be empty'}));
+//     }
+//   const post = {
+//     id: uuid(),
+//     createdAt: new Date().toISOString(),
+//     userId: 1,
+//     name: reqBody.name,
+//     email: reqBody.email,
+//     password: reqBody.password,
+//   };
+  
+//   return db.put({
+//       TableName: postsTable,
+//       Item: post
+//     }).promise().then(() => {
+//       callback(null, response(201, post));
+//     }).catch((err) => response(null, response(err.statusCode, err)));
+
+// };
 
 
 // module.exports.getItemFromDB(String)
@@ -135,4 +149,4 @@ module.exports.createUser = (event, context, callback) => {
 // return dynamoDB.get(params).promise().then(res => res.Item).catch(err => err);
 // }
 
-  
+}
