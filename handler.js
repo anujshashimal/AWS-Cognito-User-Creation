8eb47ad2458edb410ae26a8a897dd3ed22192bb2
unknown <anujshashimal456@gmail.com>
2020-02-28 14:42:57 +0530

    Modified some changes

--- handler.js
+++ handler.js
@@ -7,39 +7,32 @@ const uuid = require('uuid/v4');
 const postsTable = process.env.POSTS_TABLE;
 const attributeList = [];
 global.fetch = require('node-fetch');
-const qs = require('querystring');
-// const CognitoUserAttribute = require('amazon-cognito-identity-js')
+const lamda = new AWS.Lamda()
 
 module.exports.testing = function(event) {  
+const data = JSON.parse(JSON.stringify(event))
 
-// const data = JSON.stringify(event)
+//adding Trigger
+  // const parm = {
+  //   FunctinName : 'UserVerification',
+  //   InvocationType : 'RequestResponse',
+  //   LogType: 'Tail'
+  // }
+  
+      const username = {Name : 'given_name',Value : data.name};
+      const email = {Name : 'email',Value : data.email};
+      const mobileNum = {Name : 'phone_number',Value : data.mobileNum};
 
-    const username = {
-      Name : 'given_name',
-      Value : 'Anuj'
-     };
-     const email = {
-      Name : 'email',
-      Value : 'anujshashimal45@gmail.com'
-      };
-      const mobileNum = {
-        Name : 'phone_number',
-        Value : '+973673823'
-        };
-
-  const poolData = {
-    UserPoolId: 'us-east-1_VgnmxXYMF',
-    ClientId: '2gnb5hpsb4arauqukv12pjlac'
-  };
-
+  const poolData = {UserPoolId: 'us-east-1_VgnmxXYMF',ClientId: '2gnb5hpsb4arauqukv12pjlac'};
   console.log(event)
+
   const userPool = new AWSCognito.CognitoUserPool(poolData);
   const userData = {
-    Username: email,
+    Username: data.email,
     Pool: userPool
 }
-  const CognitoUserAttribute = new AWSCognito.CognitoUser(userData);
 
+  const CognitoUserAttribute = new AWSCognito.CognitoUser(userData);
 const attributeName = new AWSCognito.CognitoUserAttribute(username);
 const attributeEmail = new AWSCognito.CognitoUserAttribute(email);
 // const attributePassword = new AWSCognito.CognitoUserAttribute(password);
@@ -52,27 +45,50 @@ attributeList.push(attributeMobile)
 
 console.log(attributeName)
 
-userPool.signUp('username', 'password' , attributeList, null , function(err,result) {
+userPool.signUp(data.username, data.password , attributeList, null , function(err,result) {
 	if (err) {
     console.log(err)
     return;
 	}
 	var cognitoUser = result.user;
-	console.log('user name is ' + cognitoUser.getUsername());
+  console.log('user name is ' + cognitoUser.getUsername());
+  
+  // lamda.invoke(parm, (err, result) => {
+  //   if(err){
+  //     console.log('Error')
+  //   }else{
+  //     console.log('Success!')
+  //   }
+
+  // })
 })
 
-var cognitoUser = new AWSCognito.CognitoUser(userData);
-cognitoUser.confirmRegistration('015366', true, function(err, result) {
+//User verification code 
+}
 
-	if (err) {
+module.exports.auth = async(event) => {
+  const data = JSON.parse(JSON.stringify(event))
+
+  const poolData = {UserPoolId: 'us-east-1_VgnmxXYMF',ClientId: '2gnb5hpsb4arauqukv12pjlac'};
+  console.log(event)
+
+  const userPool = new AWSCognito.CognitoUserPool(poolData);
+  const userData = {
+    Username: data.email,
+    Pool: userPool
+}
+
+  const CognitoUserAttribute = new AWSCognito.CognitoUser(userData);
+
+console.log(data.verifyNum)
+cognitoUser.confirmRegistration(data.verifyNum, true, function(err, result) {
+
+  if (err) {
     console.log(err)
     return;
-	}
-	console.log('call result: ' + result);
-});
-
-
-}
+  }
+  console.log('call result: ' + result);
+});}
 
   module.exports.signup = async (callback) => {
  try{
@@ -145,8 +161,7 @@ module.exports.createUser = async (event, callback) => {
   const reqBody = JSON.parse(event.body);
 
   if (!reqBody.name || reqBody.name.trim() === '' || !reqBody.email || reqBody.email.trim() === '' || !reqBody.password || reqBody.password.trim() === '' ) {
-    return callback(null,response(400, {error: 'Post must enter you name and email and they must not be empty'}));
-    }
+    return callback(null,response(400, {error: 'Post must enter you name and email and they must not be empty'}));}  
   const post = {
     id: uuid(),
     createdAt: new Date().toISOString(),
@@ -165,4 +180,18 @@ module.exports.createUser = async (event, callback) => {
 };
 
 
-
+exports.tester = async (event) => {
+  switch (event.httpMethod) {
+      case 'POST':
+          const data = JSON.parse(event.body)
+          console.log(data)
+          break;
+      default:
+          throw new Error(`Unsupported method "${event.httpMethod}"`);
+  }
+  return {
+      statusCode: 200,
+      body: JSON.stringify({message: 'Success'})
+      
+  }
+};
