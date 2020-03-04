7e706f33524c55c327f5e2f0b8ff97270e3a4247
unknown <anujshashimal456@gmail.com>
2020-03-04 10:27:09 +0530

    Modified - 02

--- handler.js
+++ handler.js
@@ -1,94 +1,111 @@
 'use strict';
 const AWS = require('aws-sdk');
+require('cross-fetch/polyfill');
 const AWSCognito = require('amazon-cognito-identity-js')
 const CognitoUserPool = AWSCognito.CognitoUserPool;
 const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 const uuid = require('uuid/v4');
 const postsTable = process.env.POSTS_TABLE;
+const cognitoEnvClientID = process.env.UserPoolId
+const cognitoEnvPoolId = process.env.ClientId
 const attributeList = [];
 global.fetch = require('node-fetch');
-const lamda = new AWS.Lamda()
+AWS.config.apiVersions = {
+  cognitoidentityserviceprovider: '2016-04-18',
+};
 
-module.exports.testing = function(event) {  
-const data = JSON.parse(JSON.stringify(event))
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:b7ac9d19-027a-49f5-ad7b-856e4433b2c9',
+});
 
-//adding Trigger
-  // const parm = {
-  //   FunctinName : 'UserVerification',
-  //   InvocationType : 'RequestResponse',
-  //   LogType: 'Tail'
-  // }
-  
+AWS.config.apiVersions = {
+  cognitoidentityserviceprovider: '2016-04-18',
+  // other service API versions
+};
+
+
+function createResponse(status, body) {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: status,
+    body: JSON.stringify(body)
+  }
+}
+
+
+//-----------------------function1-----------------------
+const poolData = {UserPoolId: cognitoEnvClientID, ClientId: cognitoEnvPoolId};
+
+module.exports.testing = function(event, context, callback) {
+
+      const data = JSON.parse(JSON.stringify(event))
       const username = {Name : 'given_name',Value : data.name};
       const email = {Name : 'email',Value : data.email};
       const mobileNum = {Name : 'phone_number',Value : data.mobileNum};
 
-  const poolData = {UserPoolId: 'us-east-1_VgnmxXYMF',ClientId: '2gnb5hpsb4arauqukv12pjlac'};
-  console.log(event)
+      console.log(event)
 
-  const userPool = new AWSCognito.CognitoUserPool(poolData);
-  const userData = {
-    Username: data.email,
-    Pool: userPool
-}
+      const userPool = new AWSCognito.CognitoUserPool(poolData);
+      const userData = {
+        Username: data.email,
+        Pool: userPool}
 
-  const CognitoUserAttribute = new AWSCognito.CognitoUser(userData);
-const attributeName = new AWSCognito.CognitoUserAttribute(username);
-const attributeEmail = new AWSCognito.CognitoUserAttribute(email);
+      const CognitoUserAttribute = new AWSCognito.CognitoUser(userData);
+      const attributeName = new AWSCognito.CognitoUserAttribute(username);
+      const attributeEmail = new AWSCognito.CognitoUserAttribute(email);
 // const attributePassword = new AWSCognito.CognitoUserAttribute(password);
-const attributeMobile= new AWSCognito.CognitoUserAttribute(mobileNum);
+      const attributeMobile= new AWSCognito.CognitoUserAttribute(mobileNum);
 
-attributeList.push(attributeName);
-attributeList.push(attributeEmail);
+      attributeList.push(attributeName);
+      attributeList.push(attributeEmail);
 // attributeList.push(attributePassword);
-attributeList.push(attributeMobile)
+      attributeList.push(attributeMobile)
 
-console.log(attributeName)
+      console.log(attributeName)
 
-userPool.signUp(data.username, data.password , attributeList, null , function(err,result) {
-	if (err) {
-    console.log(err)
-    return;
-	}
-	var cognitoUser = result.user;
-  console.log('user name is ' + cognitoUser.getUsername());
-  
-  // lamda.invoke(parm, (err, result) => {
-  //   if(err){
-  //     console.log('Error')
-  //   }else{
-  //     console.log('Success!')
-  //   }
-
-  // })
-})
-
-//User verification code 
+      userPool.signUp(data.username, data.password , attributeList, null , function(err,result) {
+	      if (err) {
+          return callback(null, createResponse(404, 'Error'))
+        }
+	    const cognitoUser = result.user;
+          console.log('username is ' + cognitoUser.getUsername());
+          return callback(null, createResponse(200,'Success!'))
+      })
 }
+
+
+//-----------------------function2-----------------------
 
 module.exports.auth = async(event) => {
-  const data = JSON.parse(JSON.stringify(event))
 
-  const poolData = {UserPoolId: 'us-east-1_VgnmxXYMF',ClientId: '2gnb5hpsb4arauqukv12pjlac'};
-  console.log(event)
+console.log('First!',event)
+  const data = JSON.parse(event.body)
 
+  const poolData = {UserPoolId: cognitoEnvClientID, ClientId: cognitoEnvPoolId};
   const userPool = new AWSCognito.CognitoUserPool(poolData);
   const userData = {
     Username: data.email,
-    Pool: userPool
+    Pool: userPool,
+  };
+
+  const cognitoUser = new AWSCognito.CognitoUser(userData);
+
+  cognitoUser.confirmRegistration(data.verifyNum, true, function(error, result) {
+    if (error) {
+      console.log("Something went wrong!")
+      return send('Error')
+    }
+    console.log('call result: ' + result);
+    return responseHelper.send('Success!')
+
+  });
 }
 
-  const CognitoUserAttribute = new AWSCognito.CognitoUser(userData);
+//-----------------------function3-----------------------
 
-console.log(data.verifyNum)
-cognitoUser.confirmRegistration(data.verifyNum, true, function(err, result) {
-
-  if (err) {
-    console.log(err)
-    return;
-  }
-  console.log('call result: ' + result);
-});}
 
   module.exports.signup = async (callback) => {
  try{
@@ -131,6 +148,10 @@ function response(statusCode, message) {
   };
 }
 
+
+//-----------------------function4-----------------------
+
+
 module.exports.login = async (event, context) => {
   const data = JSON.parse(event.body);
   console.log(data)
@@ -141,21 +162,23 @@ module.exports.login = async (event, context) => {
     ClientId: '5cuuf1r7jjm2mh9toqa7aottoe'
   };
 
-var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-var userData = {
-	Username: data.username,
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+const userData = {
+	Username: '',
 	pool: userPool,
 };
 
-var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-cognitoUser.confirmRegistration('123456', true, function(err, result) {
+const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+cognitoUser.confirmRegistration('739051', true, function(err, result) {
 	if (err) {
 		alert(err.message || JSON.stringify(err));
 		return;}
 	console.log('call result: ' + result);
 })}
 
-// Creating a user
+
+//-----------------------function5-----------------------
+
 
 module.exports.createUser = async (event, callback) => {
   const reqBody = JSON.parse(event.body);
@@ -165,7 +188,6 @@ module.exports.createUser = async (event, callback) => {
   const post = {
     id: uuid(),
     createdAt: new Date().toISOString(),
-    userId: 1,
     name: reqBody.name,
     email: reqBody.email,
     password: reqBody.password,
@@ -178,6 +200,11 @@ module.exports.createUser = async (event, callback) => {
       callback(null, response(201, post));
     }).catch((err) => response(null, response(err.statusCode, err)));  
 };
+
+
+
+//-----------------------function6-----------------------
+
 
 
 exports.tester = async (event) => {
@@ -195,3 +222,39 @@ exports.tester = async (event) => {
       
   }
 };
+
+
+//-----------------------function7-----------------------
+
+
+module.exports.getUser = async (event)  => {
+  const data = JSON.parse(event)
+
+  const poolData = {
+    UserPoolId: 'us-east-1_UpmO45G8o',
+    ClientId: '2gnb5hpsb4arauqukv12pjlac'
+  };
+
+  const userPool = new AWSCognito.CognitoUserPool(poolData);
+
+  const userData = {
+    Username: 'anujjayasekara102',
+    pool: userPool,
+  };
+
+  const cognitoUser = new AWSCognito.CognitoUser(userData);
+  try{
+  cognitoUser.getUserAttributes(function(err, result) {
+    
+    if (err) {
+      alert(err.message || JSON.stringify(err));
+      return;
+      }
+    for (i = 0; i < result.length; i++) {
+      console.log('attribute ' + result[i].getName() + ' has value ' + result[i].getValue());
+    }
+  });
+  } catch(err){
+    console.log(err)
+  }
+}
